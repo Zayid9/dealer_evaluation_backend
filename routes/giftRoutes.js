a718b2a9f71c3db71eb67ba49261c57b21f7756e
zayid9 <maahirzein08@gmail.com>
2025-02-05 10:04:11 +0300

    Added routes for /api/gifts and /api/gifts/:id

--- routes/giftRoutes.js
+++ routes/giftRoutes.js
@@ -15,4 +15,23 @@ router.get('/:id', async (req, res) => {
     res.json(gift);
 });
 
+// Get all gifts
+router.get('/api/gifts', async (req, res) => {
+    const db = await connectToDatabase();
+    const gifts = await db.collection('gifts').find().toArray();
+    res.json(gifts);
+});
+
+
+// Get a single gift by ID
+router.get('/api/gifts/:id', async (req, res) => {
+    const db = await connectToDatabase();
+    const gift = await db.collection('gifts').findOne({ _id: new require('mongodb').ObjectId(req.params.id) });
+    if (gift) {
+        res.json(gift);
+    } else {
+        res.status(404).json({ message: "Gift not found" });
+    }
+});
+
 module.exports = router;
